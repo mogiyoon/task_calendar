32d2358b89ea5f6e8b159ff08803ca16839d27e0
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-08-28 23:00:28 +0900

    feat: update calendar design

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,8 @@
+import { fontFamily } from 'html2canvas/dist/types/css/property-descriptors/font-family';
+import { fontStyle } from 'html2canvas/dist/types/css/property-descriptors/font-style';
 import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ColorValue, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 /*
 Date Component
@@ -11,7 +14,7 @@ interface CalendarDateProps {
   setFocusedDate: React.Dispatch<React.SetStateAction<Date|null>>
 }
 
-export const CalendarDate: React.FC<CalendarDateProps> = ({detailDate, focusedDate, nowMonth, setFocusedDate}) => {
+const CalendarDate: React.FC<CalendarDateProps> = ({detailDate, focusedDate, nowMonth, setFocusedDate}) => {
   return (
     <TouchableOpacity
       onPress={() => setFocusedDate(detailDate)}
@@ -62,7 +65,7 @@ interface CalendarWeekProps {
   setFocusedDate: React.Dispatch<React.SetStateAction<Date|null>>
 }
 
-export const CalendarWeek: React.FC<CalendarWeekProps> = ({dateList, focusedDate, nowMonth, setFocusedDate}) => {
+const CalendarWeek: React.FC<CalendarWeekProps> = ({dateList, focusedDate, nowMonth, setFocusedDate}) => {
   return (
     <View style={weekContainerStyles.container}>
       {dateList.map((date) => (
@@ -76,23 +79,19 @@ const weekContainerStyles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    paddingVertical: 8
+    marginVertical: 8
   }
 })
 
 /*
 Month Component
 */
-
-/*
-Calendar
-*/
-interface CalendarProps {
+interface CalendarMonthProps {
   year: number
   month: number
 }
 
-export const Calendar: React.FC<CalendarProps> = ({year, month}) => {
+export const CalendarMonth: React.FC<CalendarMonthProps> = ({year, month}) => {
   const [focusedDate, setFocusedDate] = useState<Date | null>(null);
 
   //첫 번째 날짜
@@ -137,3 +136,140 @@ const monthContainerStyles = StyleSheet.create({
   container: {
   }
 })
+
+/*
+Calendar Head
+*/
+interface CalendarHeadProps {
+  year: number;
+  setYear:  React.Dispatch<React.SetStateAction<number>>
+  month: number;
+  setMonth:  React.Dispatch<React.SetStateAction<number>>
+}
+
+const CalendarHead: React.FC<CalendarHeadProps> = ({year, month, setYear, setMonth}) => {
+  const tmpDate = new Date(year, month);
+  const iconColor = '#3aa0ff';
+  const iconSize = 20;
+
+  const handlePrevMonth = () => {
+    if (month === 0) {
+      setYear((state) => state - 1);
+      setMonth(11);
+    } else {
+      setMonth((state) => state - 1);
+    }
+  }
+  const handleNextMonth = () => {
+    if (month === 11) {
+      setYear((state) => state + 1);
+      setMonth(0);
+    } else {
+      setMonth((state) => state + 1);
+    }
+  }
+
+  return (
+    <View style={headStyles.container}>
+      <TouchableOpacity onPress={handlePrevMonth}>
+        <Ionicons name="chevron-back" color={iconColor} size={iconSize}/>
+      </TouchableOpacity>
+      <Text style={headStyles.fontStyle}>
+        {tmpDate.toLocaleString('en-US', { month: 'long' })} {tmpDate.getFullYear()}
+      </Text>
+      <TouchableOpacity onPress={handleNextMonth}>
+        <Ionicons name="chevron-forward" color={iconColor} size={iconSize}/>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const headStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    marginVertical: 8,
+  },
+  fontStyle: {
+    fontSize: 16
+  },
+})
+
+/*
+Calendar Day
+*/
+interface CalendarDayProps {
+  day: string;
+}
+
+const CalendarDay: React.FC<CalendarDayProps> = ({day}) => {
+  let textColor: string = '#7a7a7a';
+  if (day === 'Sun') {
+    textColor = '#ff0000'
+  } else if (day === 'Sat') {
+    textColor = '#0000ff'
+  }
+
+  return (
+    <View style={dayStyles.container}>
+      <Text style={[dayStyles.text, {color: textColor}]}>
+        {day}
+      </Text>
+    </View>
+  )
+}
+
+const dayStyles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 12,
+  }
+})
+
+/*
+Calendar Day Week
+*/
+const CalendarDayWeek: React.FC = () => {
+  const dayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  return (
+    <View style={dayWeekStyles.container}>
+      {dayList.map((day) => (
+        <CalendarDay day={day} />
+      ))}
+    </View>
+  )
+}
+
+const dayWeekStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginVertical: 8
+  },
+})
+
+
+/*
+Calendar
+*/
+export const Calendar: React.FC = () => {
+  const today = new Date();
+  const [year, setYear] = useState<number>(today.getFullYear());
+  const [month, setMonth] = useState<number>(today.getMonth());
+
+  return (
+    <View>
+      <CalendarHead year={year} month={month} setYear={setYear} setMonth={setMonth} />
+      <CalendarDayWeek/>
+      <CalendarMonth year={year} month={month} />
+    </View>
+  )
+}
--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -4,12 +4,10 @@ import { Calendar } from '../components/Calendar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 function CalendarScreen() {
-  const testDate = new Date(2025, 7, 1);
-
   return (
     <SafeAreaView>
       <View>
-        <Calendar year={testDate.getFullYear()} month={testDate.getMonth()}/>
+        <Calendar/>
       </View>
     </SafeAreaView>
   );
